3389e3dad69fcb767b059ca8323b0a0fd69b8289
Naomi <naomi.paulmin@ynov.com>
2018-05-02 13:25:06 +0200

    Correction error missing intent

--- index.js
+++ index.js
@@ -52,11 +52,11 @@ app.get('/webhook', (req, res) => {
 
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
-    let value = received_message.nlp.entities.intent[0]["value"];
-    let confidence = received_message.nlp.entities.intent[0]["confidence"];
     let response;
     console.log(received_message.nlp.entities);
     if (received_message.text) {
+        let value = received_message.nlp.entities.intent[0]["value"];
+        let confidence = received_message.nlp.entities.intent[0]["confidence"];
         if(confidence && confidence > 0.8){
             if(value == 'Greetings' && confidence > 0.8) {
                 response = {
