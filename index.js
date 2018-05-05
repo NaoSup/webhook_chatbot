0265ba22f24678ede8b58e9967e0c258589def4e
Naomi <naomi.paulmin@ynov.com>
2018-05-05 20:28:43 +0200

    [Added]ESLint

--- index.js
+++ index.js
@@ -1,217 +1,223 @@
-'use strict'
-
 const express = require('express');
+
 const bodyParser = require('body-parser');
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+
 const request = require('request');
+
 const fs = require('fs');
+
 const util = require('util');
+
 const readFile = util.promisify(fs.readFile);
 
-//Creates express http server
+const { PAGE_ACCESS_TOKEN } = process.env.PAGE_ACCESS_TOKEN;
+const { WEATHER_API_KEY } = process.env.WEATHER_API_KEY;
+
+// Creates express http server
 const app = express().use(bodyParser.json());
 
-//Sets server port and logs message on success
+// Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening...'));
 
-//Creates the endpoint for the webhook
-app.post('/webhook', (req, res) => {
-    if(req.body.object === 'page') {
-        req.body.entry.forEach(entry => {
-            let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
-            //Gets the user ID
-            let sender_psid = webhook_event.sender.id;
-            senderAction(sender_psid);
-            if (webhook_event.message) {
-                handleMessage(sender_psid, webhook_event.message);
-            } else if (webhook_event.postback) {
-                handlePostback(sender_psid, webhook_event.postback);
-            }            
-        });
-        res.status(200).send('EVENT_RECEIVED');
+// Sends response via the Send API
+function callSendAPI(REQUEST_BODY) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: REQUEST_BODY,
+  }, (err) => {
+    if (!err) {
+      console.log('message sent! ');
+      // console.log(request_body.message);
     } else {
-        res.sendStatus(404);
+      console.error(`Unable to send message:${err}`);
     }
-});
+  });
+};
 
-app.get('/webhook', (req, res) => {
-    const VERIFY_TOKEN = "xUf8Fr6NMaRAfTY41zBvFPDQIssJRjLf";
-
-    let mode = req.query['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
-
-    if(mode && token) {
-        if(mode === 'subscribe' && token === VERIFY_TOKEN) {
-            console.log('WEBHOOK_VERIFIED');
-            res.status(200).send(challenge);
-        } else {
-            res.sendStatus(403);
-        }
-    }
-});
-
-//Send sender action typing on
-function senderAction(sender_psid){
-    let request_body = {
-        "messaging_type": "RESPONSE",
-        "recipient":{
-          "id": sender_psid
-        },
-        "sender_action":"typing_on"
-    }
-    callSendAPI(request_body);
-}
-
-
-//Gets the data from the weather API
-function getApiData(){
-    let url = 'http://api.openweathermap.org/data/2.5/forecast?q=Nanterre,fr&units=metric&mode=json&lang=fr&APPID='+WEATHER_API_KEY;
-    return new Promise((resolve, reject) => {
-        request.get(url, (err, response, body) => {
-            if(err){
-                reject(err);
-            } else {
-                resolve(JSON.parse(body));
-            }
-        });       
+// Send sender action typing on
+function senderAction(SENDER_PSID) {
+  const REQUEST_BODY = {
+    messaging_type: 'RESPONSE',
+    recipient: {
+      id: SENDER_PSID,
+    },
+    sender_action: 'typing_on',
+  };
+  callSendAPI(REQUEST_BODY);
+};
+  
+  
+// Gets the data from the weather API
+function getApiData() {
+  const url = `http://api.openweathermap.org/data/2.5/forecast?q=Nanterre,fr&units=metric&mode=json&lang=fr&APPID=${WEATHER_API_KEY}`;
+  return new Promise((resolve, reject) => {
+    request.get(url, (err, response, body) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(JSON.parse(body));
+      }
     });
-}
+  });
+};
+  
+// Gets the best weather condition of the week
+async function getBestWeather() {
+  let bestDay;
+  const json = await getApiData();
+  const list = json.list;
+  let fullDate = new Date(list[0].dt_txt);
+  let temp = list[0].main.temp_max;
 
-//Gets the best weather condition of the week
-async function getBestWeather(){
-    let bestDay;
-    const json = await getApiData();
-    const list = json.list;
-    let fullDate = new Date(list[0].dt_txt);
-    let temp = list[0].main.temp_max;
-        
-    for(var i = 1; i < list.length; i++){
-        let listDate = new Date(list[i].dt_txt); 
-        if(list[i].main.temp_max >= temp && listDate.getDay() != 6 && listDate.getDay() != 0){
-            temp = list[i].main.temp_max;
-            fullDate = listDate;
-        }
+  for (var i = 1; i < list.length; i++) {
+    const listDate = new Date(list[i].dt_txt);
+    if (list[i].main.temp_max >= temp && listDate.getDay() != 6 && listDate.getDay() != 0) {
+      temp = list[i].main.temp_max;
+      fullDate = listDate;
     }
-        
-    //Gets the day of the week
-    const weekdays = ["dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi"];
-    let day;
-    for(var i = 0; i < weekdays.length; i++) {
-        if(fullDate.getDay() == i) {
-            day = weekdays[i];
-        }
+  }
+  
+  // Gets the day of the week
+  const weekdays = ['dimanche', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi'];
+  let day;
+  for (var i = 0; i < weekdays.length; i++) {
+    if (fullDate.getDay() == i) {
+      day = weekdays[i];
     }
-
-    //Gets the date
-    let date = fullDate.getDate();
-
-    //Gets the month
-    const months = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"];
+  }
+  
+  // Gets the date
+  const date = fullDate.getDate();
+  
+  // Gets the month
+  const months = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'];
     let month;
-    for(var i = 0; i < months.length; i++) {
-        if(fullDate.getMonth() == i) {
-            month = months[i];
-        }
+    for (var i = 0; i < months.length; i++) {
+      if (fullDate.getMonth() == i) {
+        month = months[i];
+      }
     }
-
-    //Sends best day data as json
+  
+    // Sends best day data as json
     bestDay = {
-        "day": day,
-        "date": date,
-        "month": month,
-        "temp": temp + "C°"
-    }
-    return bestDay;
-}
-
-//Finds the response corresponding with the intent
-async function getIntentResponse(value, intents){
-    let jsonBestDay = await getBestWeather();
-    let response;
-    for(var intent in intents) {
-        if(intent == value){
-            if(intent == 'school_location') {
-                let day = jsonBestDay.day;
-                let date = jsonBestDay.date;
-                let month = jsonBestDay.month;
-                let temp = jsonBestDay.temp;
-                let text = intents[intent][Math.floor(Math.random()*intents[intent].length)] + "Vous pouvez venir nous rendre visite le " + day + " " + date + " " + month + ". Ca sera le jour le plus chaud de la semaine avec " + temp;
-                response = {
-                    "text": text
-                }
-            } else {
-                response = {
-                    "text": intents[intent][Math.floor(Math.random()*intents[intent].length)]
-                }
-            }
-        }
-        
+      day,
+      date,
+      month,
+      temp: `${temp}C°`,
     };
-
+    return bestDay;
+  };
+ 
+  // Finds the response corresponding with the intent
+  async function getIntentResponse(value, intents) {
+  const jsonBestDay = await getBestWeather();
+  let response;
+  for (const intent in intents) {
+    if (intent == value) {
+      if (intent == 'school_location') {
+        const day = jsonBestDay.day;
+        const date = jsonBestDay.date;
+        const month = jsonBestDay.month;
+        const temp = jsonBestDay.temp;
+        const text = `${intents[intent][Math.floor(Math.random() * intents[intent].length)]}Vous pouvez venir nous rendre visite le ${day} ${date} ${month}. Ca sera le jour le plus chaud de la semaine avec ${temp}`;
+        response = {
+          text,
+          };
+        } else {
+          response = {
+            text: intents[intent][Math.floor(Math.random() * intents[intent].length)],
+          };
+        }
+      }
+    } 
     return response;
-}
-
-// Handles messages events
-async function handleMessage(sender_psid, received_message) {
+  };
+  
+  // Handles messages events
+  async function handleMessage(SENDER_PSID, received_message) {
     let value;
     let confidence;
     let response;
     const fileContent = await readFile('json/intents.json');
     const intents = JSON.parse(fileContent);
-    
+  
     console.log(received_message.nlp.entities);
-    
+  
     if (received_message.text) {
-        // Verifies if there is an intent
-        if (received_message.nlp.entities.intent) {
-            value = received_message.nlp.entities.intent[0]["value"];
-            confidence = received_message.nlp.entities.intent[0]["confidence"];
-        }
-
-        //Checks if the intent is known
-        response = await getIntentResponse(value, intents);
-        //Default response
-        if(response == null) {
-            response = {
-                "text": "Je n'ai pas bien compris votre demande..."
-            }
-        }
+      // Verifies if there is an intent
+      if (received_message.nlp.entities.intent) {
+        value = received_message.nlp.entities.intent[0].value;
+        confidence = received_message.nlp.entities.intent[0].confidence;
+      }
+  
+      // Checks if the intent is known
+      response = await getIntentResponse(value, intents);
+      // Default response
+      if (response == null) {
+        response = {
+          text: "Je n'ai pas bien compris votre demande...",
+        };
+      }
     }
     console.log(response);
     // Construct the message body
-    let request_body = {
-        "messaging_type": "RESPONSE",
-        "recipient": {
-            "id": sender_psid
-        },
-        "message": response
-    }
-    
+    const REQUEST_BODY = {
+      messaging_type: 'RESPONSE',
+      recipient: {
+        id: SENDER_PSID,
+      },
+      message: response,
+    };
+  
     // Sends the response message
-    callSendAPI(request_body);    
+    callSendAPI(REQUEST_BODY);
+  };
+  
+  // Handles messaging_postbacks events
+  function handlePostback(SENDER_PSID, received_postback) {
+  
+  };
+
+// Creates the endpoint for the webhook
+app.post('/webhook', (req, res) => {
+  if (req.body.object === 'page') {
+    req.body.entry.forEach((entry) => {
+      const WEBHOOK_EVENT = entry.messaging[0];
+      console.log(WEBHOOK_EVENT);
+      // Gets the user ID
+      const SENDER_PSID = WEBHOOK_EVENT.sender.id;
+
+      // Sender action
+      senderAction(SENDER_PSID);
+
+      // Handles sender response
+      if (WEBHOOK_EVENT.message) {
+        handleMessage(SENDER_PSID, WEBHOOK_EVENT.message);
+      } else if (WEBHOOK_EVENT.postback) {
+        handlePostback(SENDER_PSID, WEBHOOK_EVENT.postback);
+      }
+    });
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    res.sendStatus(404);
   }
+});
 
-// Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {
+app.get('/webhook', (req, res) => {
+  const VERIFY_TOKEN = 'xUf8Fr6NMaRAfTY41zBvFPDQIssJRjLf';
 
-}
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
 
-// Sends response via the Send API
-function callSendAPI(request_body) {
-    request({
-        "uri": "https://graph.facebook.com/v2.6/me/messages",
-        "qs": { "access_token": PAGE_ACCESS_TOKEN },
-        "method": "POST",
-        "json": request_body
-    }, (err, res, body) => {
-        if (!err) {
-            console.log('message sent! ');
-            //console.log(request_body.message);
-        } else {
-            console.error("Unable to send message:" + err);
-        }
-  }); 
-}
+  if (mode && token) {
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    } else {
+      res.sendStatus(403);
+    }
+  }
+});
